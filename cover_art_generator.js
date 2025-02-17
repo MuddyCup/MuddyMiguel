0d4dd02caac5858b8a5667af7fbb727e7923efc6
MuddyCup <tommyjstepan@gmail.com>
2025-02-16 21:39:46 -0500

    Update cover_art_generator.js

--- cover_art_generator.js
+++ cover_art_generator.js
@@ -1,47 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const layers = ["background", "miguel", "title", "lv", "shirt"];
-    
-    // Function to load available image options from /assets/ folder
-    function loadOptions() {
-        layers.forEach(layer => {
+    const layers = {
+        background: ["bg1.png", "bg2.png"],
+        miguel: ["miguel1.png", "miguel2.png"],
+        title: ["title1.png", "title2.png"],
+        lv: ["lv1.png", "lv2.png"],
+        shirt: ["shirt1.png", "shirt2.png"]
+    };
+
+    function populateDropdowns() {
+        Object.keys(layers).forEach(layer => {
             const selectElement = document.getElementById(`${layer}Select`);
-            fetch(`assets/${layer}/`) // Assumes directory browsing is enabled (may not work on GitHub Pages)
-                .then(response => response.text())
-                .then(text => {
-                    const parser = new DOMParser();
-                    const htmlDoc = parser.parseFromString(text, "text/html");
-                    const options = Array.from(htmlDoc.querySelectorAll("a"))
-                        .map(link => link.href.split("/").pop())
-                        .filter(name => name.endsWith(".png"));
-                    
-                    options.forEach(option => {
-                        let opt = document.createElement("option");
-                        opt.value = option;
-                        opt.innerText = option;
-                        selectElement.appendChild(opt);
-                    });
-                    
-                    selectElement.addEventListener("change", () => updateLayer(layer, selectElement.value));
-                })
-                .catch(error => console.error(`Error loading ${layer} options:`, error));
+            layers[layer].forEach(file => {
+                let option = document.createElement("option");
+                option.value = file;
+                option.textContent = file;
+                selectElement.appendChild(option);
+            });
+            selectElement.addEventListener("change", () => updateLayer(layer, selectElement.value));
         });
     }
-    
-    // Function to update image layers when selection changes
+
     function updateLayer(layer, file) {
-        const imgElement = document.getElementById(layer);
-        imgElement.src = `assets/${layer}/${file}`;
+        document.getElementById(layer).src = `assets/${layer}/${file}`;
     }
-    
-    // Function to merge layers and download the final image
+
     function downloadImage() {
         const canvas = document.createElement("canvas");
         canvas.width = 500;
         canvas.height = 500;
         const ctx = canvas.getContext("2d");
-        let imagesLoaded = 0;
-        let totalImages = layers.length;
-        
+
+        let images = [];
+        let loadedImages = 0;
+
         function drawImage(img, callback) {
             const image = new Image();
             image.crossOrigin = "anonymous";
@@ -51,30 +42,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 callback();
             };
         }
-        
+
         function checkCompletion() {
-            imagesLoaded++;
-            if (imagesLoaded === totalImages) {
+            loadedImages++;
+            if (loadedImages === layers.length) {
                 const link = document.createElement("a");
                 link.download = "custom_album_cover.png";
                 link.href = canvas.toDataURL("image/png");
                 link.click();
             }
         }
-        
-        layers.forEach(layer => {
+
+        Object.keys(layers).forEach(layer => {
             const img = document.getElementById(layer);
             if (img.src && img.src !== window.location.href) {
                 drawImage(img, checkCompletion);
             } else {
-                totalImages--;
+                loadedImages++;
             }
         });
     }
-    
-    // Attach the download function to the download button
+
     document.getElementById("downloadButton").addEventListener("click", downloadImage);
-    
-    // Load image options on page load
-    loadOptions();
+    populateDropdowns();
 });
