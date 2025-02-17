8e332bbdb07ba7658157f108be78e406bb0c27f3
MuddyCup <tommyjstepan@gmail.com>
2025-02-16 23:42:24 -0500

    Update cover_art_generator.js

--- cover_art_generator.js
+++ cover_art_generator.js
@@ -40,20 +40,19 @@ document.addEventListener("DOMContentLoaded", function () {
     
 
  function updateLayer(layer, file) {
-        const imgElement = document.getElementById(layer);
-        if (file === "None") {
-            imgElement.style.display = "none";
-        } else {
-            imgElement.style.display = "block";
-            imgElement.src = `assets/${layer}/${file}`;
-            imgElement.style.width = "2000px";
-            imgElement.style.height = "2000px";
-            imgElement.style.position = "absolute";
-            imgElement.style.top = "50%";
-            imgElement.style.left = "50%";
-            imgElement.style.transform = "translate(-50%, -50%)";
-        }
+    const imgElement = document.getElementById(layer);
+    if (file === "None") {
+        imgElement.style.display = "none";
+    } else {
+        imgElement.style.display = "block";
+        imgElement.src = `assets/${layer}/${file}`;
+        imgElement.style.position = "absolute";
+        imgElement.style.top = "0px";
+        imgElement.style.left = "0px";
+        imgElement.style.width = "auto";  // Ensures original width is used
+        imgElement.style.height = "auto"; // Ensures original height is used
     }
+}
 
 
     function drawImage(img, callback) {
